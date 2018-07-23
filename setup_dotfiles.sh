38404e8422802165fc180b892d418fc4e12c0fba
Kyle Bock <kylebock@Kyles-MacBook-Pro.local>
2018-07-23 18:32:10 -0400

    simplified setup script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,63 +1,25 @@
 #!/bin/bash
 
-###################
-## Vim Setup     ##
-###################
-overwrite_vim_dir=Y
-if [ -d ~/.vim ]; then
-  echo "Would you like to overwrite ~/.vim? [Yn]:"
-  read vim_dir_input
+# terminal setup
+ln -sf `pwd`/.zshrc ~/.zshrc
+ln -sf `pwd`/.bash_aliases ~/.bash_aliases
+ln -sf `pwd`/.bash_functions ~/.bash_functions
 
-  # check input string
-  if [ "$vim_dir_input" != "" ]; then
-    $overwrite_vim_dir=$vim_dir_input
-  fi
-fi
-
-if [ "$overwrite_vim_dir" == "Y" ] || [ "$overwrite_vim_dir" == "y" ]; then
-  echo "Preparing to overwrite ~/.vim"
-  rm -rf ~/.vim
-  ln -s `pwd`/.vim ~/.vim
-  echo "Overwrite of ~/.vim complete"
-fi
-
-overwrite_vimrc=Y
-if [ -f ~/.vimrc ]; then
-  echo "Would you like to overwrite your .vimrc? [Yn]:"
-  read vim_rc_input
-  if [ "$vim_rc_input" != "" ]; then
-    $overwrite_vimrc=$vim_rc_input
-  fi
-fi
-
-if [ "$overwrite_vimrc" == "Y" ] ||  [ "$overwrite_vimrc" == "y"]; then
-  echo "Preparing to overwrite ~/.vimrc"
-  rm ~/.vimrc
-  ln -s `pwd`/.vimrc ~/.vimrc
-  echo "Overwrite of ~/.vimrc complete"
-fi
-
-#################
-## bash files ###
-#################
-overwrite_bash_aliases=Y
-if [ -f ~/.bash_aliases ]; then
-  echo "Would you like to overwrite your .bash_aliases? [Yn]:"
-  read bash_a_input
-  if [ "$bash_a_input" != "" ]; then
-    $overwrite_bash_aliases=$bash_a_input
-  fi
-fi
-
-if [ "$overwrite_bash_aliases" == "Y" ] || [ "$overwrite_bash_aliases" == "y" ]; then
-  echo "Preparing to overwrite ~/.bash_aliases"
-  ln -sf `pwd`/.bash_aliases ~/.bash_aliases
-  echo "Overwrite of ~/.bash_aliases complete"
-fi
-
+# window management
 ln -sf `pwd`/.slate.js ~/.slate.js
+
+# tmux setup
 ln -sf `pwd`/.tmux.conf ~/.tmux.conf
 ln -sf `pwd`/.tmux ~/.tmux
-ln -sf `pwd`/.tmux-powerlinerc ~/.tmux-powerlinerc
+ln -sf `pwd`/.tmuxinator ~/.tmuxinator
+
+# git stuff
 ln -sf `pwd`/.gitignore ~/.gitignore
-export DEV_BAR_ENABLED=1
+ln -sf `pwd`/.gitconfig ~/.gitconfig
+ln -sf `pwd`/.gitignore_global ~/.gitignore_global
+
+# vim stuff
+ln -sf `pwd`/.vimrc ~/.vimrc
+
+# ruby stuff
+ln -sf `pwd`/.pryrc ~/.pryrc
